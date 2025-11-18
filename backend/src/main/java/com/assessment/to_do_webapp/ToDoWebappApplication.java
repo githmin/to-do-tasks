e635377b4b1dfa03a8f5ebcd673e0c40e19c119e
Githmin Jayawardhana <githmin21@gmail.com>
2025-11-18 11:47:21 +0530

    init backend

---
+++ backend/src/main/java/com/assessment/to_do_webapp/ToDoWebappApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.to_do_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebappApplication.class, args);
+	}
+
+}
