5c3647119d01dd714bbd118a32d49661fc9b2277
Githmin Jayawardhana <githmin21@gmail.com>
2025-11-18 11:56:21 +0530

    added task service

---
+++ backend/src/main/java/com/assessment/to_do_webapp/service/TaskService.java
@@ -0,0 +1,47 @@
+package com.assessment.to_do_webapp.service;
+
+import com.assessment.to_do_webapp.dto.CreateTaskRequestDTO;
+import com.assessment.to_do_webapp.dto.TaskResponseDTO;
+import com.assessment.to_do_webapp.model.Task;
+import com.assessment.to_do_webapp.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public List<TaskResponseDTO> getMostRecent5IncompletedTasks(){
+        return taskRepository.findFirst5ByIsCompletedFalseOrderByCreatedAtDesc().stream().map(task -> convertToDto(task)).collect(Collectors.toList());
+    }
+
+    public TaskResponseDTO createTask(CreateTaskRequestDTO request){
+        Task nt = new Task();
+        nt.setTitle(request.title());
+        nt.setDescription(request.description());
+        Task saved = taskRepository.save(nt);
+        return convertToDto(saved);
+    }
+
+    public TaskResponseDTO markAsComplete(UUID id) throws RuntimeException{
+        Task t = taskRepository.findById(id).orElseThrow(()-> new RuntimeException("Task Not Found"));
+        t.setIsCompleted(true);
+        Task updateTask = taskRepository.save(t);
+        return convertToDto(updateTask);
+    }
+
+    private TaskResponseDTO convertToDto(Task t){
+        return  new TaskResponseDTO(
+                t.getId(),
+                t.getTitle(),
+                t.getDescription(),
+                t.getIsCompleted(),
+                t.getCreatedAt());
+    }
+}
